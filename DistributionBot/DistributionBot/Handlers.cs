651f86b209d681ddc7430bb9e6a1dc58035b8e38
KaraVladimir2 <vova.kara1996@gmail.com>
2022-06-21 13:57:26 +0300

    Commit

---
+++ DistributionBot/DistributionBot/Handlers.cs
@@ -0,0 +1,75 @@
+﻿using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Telegram.Bot.Examples.Polling;
+
+public class Handlers 
+{
+    static List<long> chats = new List<long>();
+
+    public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var handler = update.Type switch
+        {
+            UpdateType.Message => BotOnMessageReceived(botClient, update.Message!),
+            UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage!),
+            _ => UnknownUpdateHandlerAsync(botClient, update)
+        };
+        
+        try
+        {
+            await handler;
+        }
+        catch (Exception exception)
+        {
+            await HandleErrorAsync(botClient, exception, cancellationToken);
+        }
+    }
+
+    private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+    {
+        Console.WriteLine($"Message type: {message.Type}\nMessage text: {message.Text}\nChat id: {message.Chat.Id}");
+        if (message.Type != MessageType.Text)
+            return;
+        if (message.Text == "/start") await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                                                                     text: "Hello",
+                                                                     parseMode: null
+                                                                    );
+        else if (message.Text.StartsWith("/add"))
+        {
+            long temp;
+            long.TryParse(message.Text.Remove(0, 4), out temp);
+            chats.Add(temp);                                        //-1001780914140     -568811382     -726946510
+        }
+
+        else if(message.Chat.Id == 1030464002)
+        {
+            for (int i = 0; i < chats.Count(); i++)
+            {
+                await botClient.SendTextMessageAsync(chatId: chats[i], 
+                                                 text: message.Text,
+                                                 parseMode: null);
+            }
+        }
+    }
+
+    private static Task UnknownUpdateHandlerAsync(ITelegramBotClient botClient, Update update)
+    {
+        Console.WriteLine($"Unknown update type: {update.Type}");
+        return Task.CompletedTask;
+    }
+}
---
+++ DistributionBot/DistributionBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Examples.Polling;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types.Enums;
+
+string BotToken = "5351340324:AAG_6n35fWgTXAbPJ80btAl5JY1mlqEs3lU";
+var bot = new TelegramBotClient(BotToken);
+
+var me = await bot.GetMeAsync();
+Console.Title = me.Username ?? "My awesome Bot";
+
+using var cts = new CancellationTokenSource();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+bot.StartReceiving(updateHandler: Handlers.HandleUpdateAsync,
+                   errorHandler: Handlers.HandleErrorAsync,
+                   receiverOptions: new ReceiverOptions()
+                   {
+                       AllowedUpdates = Array.Empty<UpdateType>()
+                   },
+                   cancellationToken: cts.Token);
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+// Send cancellation request to stop bot
+cts.Cancel();
